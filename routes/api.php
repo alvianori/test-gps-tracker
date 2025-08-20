a041cd2ae93870b7095a7e16e3f3926d4dfe1fa9
farhan <farhansubeno@gmail.com>
2025-08-20 14:23:13 +0700

    feat: api deteksi role

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\GpsDataController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
 
 
 
@@ -18,3 +20,23 @@ Route::apiResource('gps-data', GpsDataController::class)->except([
     'update',
     'destroy'
 ]);
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/users', [UserController::class, 'store'])->middleware('role:super_admin');
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    // contoh: cek profile user
+    Route::get('/check', function (Request $request) {
+        $user = $request->user();
+
+        return response()->json([
+            'id'          => $user->id,
+            'name'        => $user->name,
+            'email'       => $user->email,
+            'roles'       => $user->getRoleNames(),
+            'permissions' => $user->getAllPermissions()->pluck('name'),
+        ]);
+    });
+});
