d2c7ff16b73cf2efe03345f0be40ad9d2961ec93
farhan <farhansubeno@gmail.com>
2025-08-25 08:55:59 +0700

    feat: api check role

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\GpsDataController;
+use App\Http\Controllers\Api\AuthController;
 
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/check', [AuthController::class, 'check']);
+});
 
 
 Route::prefix('gps-data')->controller(GpsDataController::class)->group(function () {
